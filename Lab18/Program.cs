ffb3fcb6b9e6d2807f12a3bf54fc44b3102e26fe
avolodin1479@gmail.com <avolodin1479@gmail.com>
2021-10-26 12:49:32 +0600

    Добавьте файлы проекта.

---
+++ Lab18/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Str = { "d{{{ddfdf((((sffd)sf" };
+            Stack<string> A = new Stack<string>();
+           
+            foreach (string i in Str)
+            {
+                if (i == "(")
+                {
+                    A.Push(")");
+                }
+                if (i == "[")
+                {
+                    A.Push("]");
+                }
+                if (i == "{")
+                {
+                    A.Push("}");
+                }
+                if (i == ")" && i == A.Peek())
+                {
+                    A.Pop();
+                }
+                if (i == "]" && i == A.Peek())
+                {
+                    A.Pop();
+                }
+                if (i == "}" && i == A.Peek())
+                {
+                    A.Pop();
+                }
+            }
+            if (A.Count == 0)
+            {
+                Console.WriteLine("Корректно");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка");
+            }
+            Console.ReadKey();
+        }
+    }
+}
